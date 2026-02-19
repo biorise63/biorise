a0a1d0d9e60886b6ceb7f30c3d34d2951ce5452f
biorise63 <biorise63@users.noreply.github.com>
2026-02-19 18:28:23 +0300

    Revert "Фикс подменю Услуги: убран разрыв между кнопкой и списком"
    This reverts commit 68c8dacfc34dbf8de02ae17fa139a9d49d2cf8de.

--- components/Header.tsx
+++ components/Header.tsx
@@ -167,7 +167,7 @@ export default function Header() {
 
               {/* Услуги с подменю */}
               <div
-                className="relative pb-2"
+                className="relative"
                 ref={servicesRef}
                 onMouseEnter={() => setIsServicesOpen(true)}
                 onMouseLeave={() => setIsServicesOpen(false)}
@@ -188,7 +188,7 @@ export default function Header() {
                 </button>
                 {isServicesOpen && (
                   <div
-                    className="absolute left-0 top-full mt-0 w-48 bg-white rounded-lg shadow-premium border border-olive-primary/10 z-40 pt-2"
+                    className="absolute left-0 mt-2 w-48 bg-white rounded-lg shadow-premium border border-olive-primary/10 z-40"
                     onMouseEnter={() => setIsServicesOpen(true)}
                     onMouseLeave={() => setIsServicesOpen(false)}
                   >
