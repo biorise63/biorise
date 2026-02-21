484f6e979b6ecbdccc2e4689a0a6c18c899b80fa
biorise63 <biorise63@users.noreply.github.com>
2026-02-21 18:43:27 +0300

    UI: убрать иконку ⬇ из кнопки прайса, сделать кнопки каталога и прайса более прозрачными

--- components/kapelnicy/DownloadPdfButton.tsx
+++ components/kapelnicy/DownloadPdfButton.tsx
@@ -37,9 +37,7 @@ export default function DownloadPdfButton() {
         <polyline points="7 10 12 15 17 10" />
         <line x1="12" y1="15" x2="12" y2="3" />
       </svg>
-      <span className="download-text">
-        <span className="download-arrow">⬇</span> Прайс капельниц
-      </span>
+      <span className="download-text">Прайс капельниц</span>
     </button>
   )
 }
