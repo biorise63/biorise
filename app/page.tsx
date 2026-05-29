31a778e4cc16f349dc7bd93b3d164c33ace3a6aa
biorise63 <biorise63@users.noreply.github.com>
2026-05-29 17:45:03 +0400

    Add exit-intent offers popup with carousel and metrics

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import ClinicGallery from '@/components/ClinicGallery'
 import BookingForm from '@/components/BookingForm'
 import Reviews from '@/components/Reviews'
 import Footer from '@/components/Footer'
-import VkContestPopup from '@/components/VkContestPopup'
+import ExitIntentOffersPopup from '@/components/ExitIntentOffersPopup'
 
 export default function Home() {
   return (
@@ -21,7 +21,7 @@ export default function Home() {
       <BookingForm />
       <Reviews />
       <Footer />
-      <VkContestPopup />
+      <ExitIntentOffersPopup />
     </main>
   )
 }
